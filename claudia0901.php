bf07e0df74d937b5cc8b833b26cc296d45aaf382
claudia <claudia.matei@libero.it>
2024-01-09 20:01:58 +0100

    modifica1

---
+++ claudia0901.php
@@ -0,0 +1,35 @@
+<?php 
+//traccia 1
+/* $variabile_integer = 1 ;
+$variabile_float = 1.5 ;
+$variabile_string = "Questa è una stringa";
+$variabile_boolean1 = true;
+$variabile_boolean2 = false;
+var_dump( $variabile_integer );
+var_dump( $variabile_float );
+var_dump( $variabile_string );
+var_dump( $variabile_boolean1 );
+var_dump( $variabile_boolean2 );
+echo $variabile_integer;
+echo $variabile_string ; */
+
+//traccia 2
+
+
+/* $text1 = "Marco"; 
+$text2 = "hai"; 
+$text3 = "sete"; 
+$text4 = "?"; 
+$text5 = "Perchè"; 
+$text6 = "$text2"; 
+$text7 = 'bevuto'; 
+$text8 = "tutto";
+
+var_dump ($text1." ".$text2." ".$text3.$text4.$text5." ".$text6." ".$text7." ".$text8.".");
+
+echo $text1 ." ".$text2." ".$text3." ".$text4.$text5." ".($text6)." ".$text7." ".$text8.".";*/
+
+//traccia 3
+
+
+
